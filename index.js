a5dc0fedb1192555228f1bd5ba7dd4b0bb0e3dde
Renij Shrestha <renijshrestha8@gmail.com>
2022-08-24 20:21:25 +1000

    get

--- index.js
+++ index.js
@@ -12,6 +12,14 @@ app.get("/login", (req, res) => {
   res.sendFile(path.join(__dirname + "/public/html/Login.html"));
 });
 
+app.get("/contact", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/html/Contact.html"));
+});
+
+app.get("/reminder", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/html/reminder.html"));
+});
+
 app.listen(3000, () => {
   console.log("Listening on port 3000");
 });
