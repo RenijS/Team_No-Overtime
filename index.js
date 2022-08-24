4a17192c9cb72a7e5542f1e12d4f84676d8f4e7a
Renij Shrestha <renijshrestha8@gmail.com>
2022-08-24 11:34:40 +1000

    get added

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const path = require("path");
 
 app.use(express.static(path.join(__dirname, "public")));
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/html/index.html"));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/html/Login.html"));
+});
+
 app.listen(3000, () => {
   console.log("Listening on port 3000");
 });
