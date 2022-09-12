8e9f0ff68efb603ad63d1ad5d169b60ec695ab40
Renij Shrestha <renijshrestha8@gmail.com>
2022-09-12 11:00:16 +1000

    template engine

--- index.js
+++ index.js
@@ -9,22 +9,18 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  //res.sendFile(path.join(__dirname + "/public/html/index.html"));
   res.render("index");
 });
 
 app.get("/login", (req, res) => {
-  //res.sendFile(path.join(__dirname + "/public/html/Login.html"));
   res.render("login");
 });
 
 app.get("/contact", (req, res) => {
-  //res.sendFile(path.join(__dirname + "/public/html/Contact.html"));
   res.render("Contact");
 });
 
 app.get("/reminder", (req, res) => {
-  //res.sendFile(path.join(__dirname + "/public/html/reminder.html"));
   res.render("reminder");
 });
 
