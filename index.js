06189d3dbe13e10b101712521728f66059aa126b
Renij Shrestha <renijshrestha8@gmail.com>
2022-09-12 11:00:03 +1000

    template engine

--- index.js
+++ index.js
@@ -4,21 +4,28 @@ const port = process.env.PORT || 3000;
 const path = require("path");
 
 app.use(express.static(path.join(__dirname, "public")));
+//ejs
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "/public/html/index.html"));
+  //res.sendFile(path.join(__dirname + "/public/html/index.html"));
+  res.render("index");
 });
 
 app.get("/login", (req, res) => {
-  res.sendFile(path.join(__dirname + "/public/html/Login.html"));
+  //res.sendFile(path.join(__dirname + "/public/html/Login.html"));
+  res.render("login");
 });
 
 app.get("/contact", (req, res) => {
-  res.sendFile(path.join(__dirname + "/public/html/Contact.html"));
+  //res.sendFile(path.join(__dirname + "/public/html/Contact.html"));
+  res.render("Contact");
 });
 
 app.get("/reminder", (req, res) => {
-  res.sendFile(path.join(__dirname + "/public/html/reminder.html"));
+  //res.sendFile(path.join(__dirname + "/public/html/reminder.html"));
+  res.render("reminder");
 });
 
 app.listen(port, () => {
